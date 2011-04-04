6b87da5c8f64c557778f9f4372751bf75dacfd5b
Rick Ghanem <rickghanem@gmail.com>
2011-04-04 14:51:11 +0200

    Creating project structure .

---
+++ src/fr/mobiwide/gcalendar/MainActivity.java
@@ -0,0 +1,21 @@
+package fr.mobiwide.gcalendar;
+
+import fr.mobiwide.agenda.google.GoogleAgenda;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        
+        // Test
+        GoogleAgenda agenda = new GoogleAgenda(this);
+        agenda.listAllCalendarDetails();
+        
+        
+    }
+}
